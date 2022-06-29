de2e75a97274c8c657a3ace8ef8656a59b960f1f
ThiagoOMiranda <th_profile@hotmail.com>
2022-06-29 09:26:43 -0300

    Correção de endereços no script

--- assets/js/script.js
+++ assets/js/script.js
@@ -67,11 +67,11 @@ changeFront();
 
 function screenMobile(screenSize) {
     if (screenSize.matches) {
-        cards[0].style.backgroundImage = "url(./assets/imgs/snake.gif)";
-        cards[1].style.backgroundImage = "url(./assets/imgs/to-do.gif)";
-        cards[2].style.backgroundImage = "url(./assets/imgs/pokemem.gif)";
-        cards[3].style.backgroundImage = "url(./assets/imgs/click-counter.gif)";
-        cards[4].style.backgroundImage = "url(./assets/imgs/gameflix.gif)";
+        cards[0].style.backgroundImage = "url('./assets/imgs/snake.gif')";
+        cards[1].style.backgroundImage = "url('./assets/imgs/to-do.gif')";
+        cards[2].style.backgroundImage = "url('./assets/imgs/pokemem.gif')";
+        cards[3].style.backgroundImage = "url('./assets/imgs/click-counter.gif')";
+        cards[4].style.backgroundImage = "url('./assets/imgs/gameflix.gif')";
         links[1].addEventListener("click", () => {
             distanceScrolled = project.offsetTop;
             logoWrapper.classList.add("hidden");
