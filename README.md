bd1f2dd481eb66d9425acd3aae69b9d2291b1d95
ThiagoOMiranda <th_profile@hotmail.com>
2022-06-29 08:30:43 -0300

    primeiro commit

---
+++ README.md
@@ -0,0 +1,91 @@
+<center >
+
+### **DESAFIO PROPOSTO NO BOOTCAMP DA DIO:**
+
+</center>
+
+<h2 align="center" color="red" >
+
+***LANDING PAGE***
+
+</h2>
+
+<h3 align="center">
+
+**Criado e desenvolvido por:** *Thiago de Oliveira Miranda*
+
+</h3>
+
+***
+
+<br>
+
+&emsp;&emsp;O desafio proposto consistia em criar uma ***landing Page***, aplicando os conhecimentos aprendidos de animações em CSS. Foram explorados os conceitos de ***CSS Transitions***, utilizando pseudo elementos e pseudo classes, e ***CSS @keyframes*** para desenvolver animações com regras específicas. A página de portifólio, contém o ***hamburguer menu*** ensinado no bootcamp da DIO, além de efeitos manipulando ***filters*** nas imagens. 
+
+&emsp;&emsp;Para finalizar a página, foram inseridos links para outros projetos contidos em meu ***Github*** com um breve descritivo de cada atividade. 
+
+<br>
+
+***
+
+<br>
+
+### **Resultado esperado:**
+
+<br>
+
+<p align="center"><img width="500" src="./assets/imgs/ref.jpg"/>
+
+<p align="center">Figura 01: Landing Page</p>
+
+
+</p>
+
+<br>
+
+***
+
+<br>
+
+<h2 align="center">
+
+  **MELHORIAS IMPLEMENTADAS**
+
+</h2>
+
+### **Resultado obtido:**
+
+<br>
+
+<p align="center"><img width="500" src="./assets/imgs/resultado.jpg"/>
+
+<p align="center">Figura 02: Resultado do desafio.</p>
+
+</p>
+
+<br>
+
+<p align="center"><img width="300" src="./assets/imgs/resultado_mobile.jpg"/>
+
+<p align="center">Figura 02: Versão para dispositivos móveis.</p>
+
+</p>
+
+<br>
+
+* [x]  Background animado;
+* [x]  Card principal com efeito ***Glassmorph*** e interativo ao clique em desktops;           
+* [x]  Efeito de transição ***hover*** nos cards entre ***.gif*** e ***.jpg***.;
+* [x]  Links do menu hamburguer funcionais;
+* [x]  Efeitos sonoros nos links e menu;
+* [x]  Modificações no layout e dimensões da navbar em dispositivos mobiles;
+* [x]  Condição que monitora o evento ***scroll*** da tela para esconder a navbar em dispositivos mobile;
+* [x]  Responsividade para telas menores e dispositivos móveis;
+* [x]  O design e a disposição dos botões muda para adequar a funcionalidade para tablets e celulares;
+* [x]  Link das atividades de Keyframes & Transitions;
+* [x]  Customização da scrollbar;
+
+<br>
+
+***
+
---
+++ assets/js/script.js
@@ -0,0 +1,108 @@
+const menuToggle = document.getElementById("toggle");
+const links = document.querySelectorAll(".text-wrapper");
+const cards = document.querySelectorAll(".card");
+const mediaLinks = document.querySelectorAll("div.social-media a")
+const cardWrappers = document.querySelectorAll(".card-wrapper");
+const logoWrapper = document.getElementsByClassName("logo-wrapper")[0];
+const project = document.getElementById("projetos");
+const frontWrapper = document.getElementsByClassName("front-wrapper");
+const screenHeight = window.innerHeight;
+
+const beepSound = new Audio();
+beepSound.src = "./assets/snd/beep.mp3";
+
+let distanceScrolled = Math.ceil(window.scrollY);
+
+cards[0].addEventListener("click", () => {
+    location.href = "https://thiagoomiranda.github.io/snack-e/";
+});
+cards[1].addEventListener("click", () => {
+    location.href = "https://thiagoomiranda.github.io/to-do-list/";
+});
+cards[2].addEventListener("click", () => {
+    location.href = "https://thiagoomiranda.github.io/poke-memory/";
+});
+cards[3].addEventListener("click", () => {
+    location.href = "https://thiagoomiranda.github.io/click-counter/";
+});
+
+function unCheck() {
+    if (menuToggle.checked) {
+        menuToggle.checked = false;
+    };
+};
+
+links.forEach((element) => {
+    element.addEventListener("click", unCheck);
+    element.addEventListener("mouseover", () => {
+        setTimeout(() => {
+            beepSound.play();
+        }, 250);
+    });
+});
+
+mediaLinks.forEach((element) => {
+    element.addEventListener("mouseover", () => {
+        beepSound.play();
+    });
+});
+
+let screenSize = window.matchMedia("(max-width: 850px)");
+
+function changeFront() {
+    links[0].addEventListener("click", () => {
+        frontWrapper[1].classList.add("dnone");
+        frontWrapper[0].classList.remove("dnone");
+    });
+    links[2].addEventListener("click", () => {
+        frontWrapper[0].classList.add("dnone");
+        frontWrapper[1].classList.remove("dnone");
+    });
+};
+
+changeFront();
+
+function screenMobile(screenSize) {
+    if (screenSize.matches) {
+        cards[0].style.backgroundImage = "url('/assets/imgs/snake.gif')";
+        cards[1].style.backgroundImage = "url('/assets/imgs/to-do.gif')";
+        cards[2].style.backgroundImage = "url('/assets/imgs/pokemem.gif')";
+        cards[3].style.backgroundImage = "url('/assets/imgs/click-counter.gif')";
+        links[1].addEventListener("click", () => {
+            distanceScrolled = project.offsetTop;
+            logoWrapper.classList.add("hidden");
+        });
+
+        window.addEventListener("scroll", () => {
+            setTimeout(() => {
+                distanceScrolled = Math.ceil(window.scrollY);
+            }, 525);
+
+            if (distanceScrolled > project.offsetTop + 5 || distanceScrolled < project.offsetTop - 5) {
+                logoWrapper.classList.remove("hidden");
+            };
+        });
+
+        cards.forEach((element) => {
+            element.classList.add("card-modify");
+        });
+
+        cardWrappers.forEach((element) => {
+            element.classList.add("card-modify");
+        });
+
+    } else {
+        cards.forEach((element) => {
+            if (element.classList.contains("card-modify")) {
+                element.classList.remove("card-modify");
+            };
+        });
+        cardWrappers.forEach((element) => {
+            if (element.classList.contains("card-modify")) {
+                element.classList.remove("card-modify");
+            };
+        });
+    }
+};
+screenMobile(screenSize);
+screenSize.addListener(screenMobile);
